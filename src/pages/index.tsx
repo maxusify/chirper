3299332748f3e184d34d9f5e271deec0d7ba9341
maxusify <maxusify@gmail.com>
2023-03-23 14:00:53 +0100

    Loading states; Feed component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,13 @@ import { api, type RouterOutputs } from "@/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import { LoadingPage } from "@/components/LoadingSpinner";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
 
   if (!user) return null;
-
-  console.log(user);
 
   return (
     <div className="flex w-full gap-3">
@@ -56,19 +55,35 @@ const PostView = (props: PostWithUser) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-xl">{post.content}</span>
       </div>
     </div>
   );
 };
 
+const Feed = () => {
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+
+  if (postsLoading) return <LoadingPage />;
+
+  if (!data) return <div>Something wen wrong.</div>;
+
+  return (
+    <div className="flex flex-col">
+      {[...data, ...data]?.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
+
 const Home: NextPage = () => {
-  const user = useUser();
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  const { data, isLoading } = api.posts.getAll.useQuery();
+  // Start fetching as soon as possible
+  api.posts.getAll.useQuery();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (!data) return <div>Something went wrong.</div>;
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -80,19 +95,16 @@ const Home: NextPage = () => {
       <main className="flex h-screen justify-center ">
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
-            <div>{!user.isSignedIn && <SignInButton />}</div>
-            {!!user.isSignedIn && (
+            <div>{!isSignedIn && <SignInButton />}</div>
+            {!!isSignedIn && (
               <div className="flex w-full">
                 <CreatePostWizard />
                 {/* <SignOutButton /> */}
               </div>
             )}
           </div>
-          <div className="flex flex-col">
-            {[...data, ...data]?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-          </div>
+
+          <Feed />
         </div>
       </main>
     </>
