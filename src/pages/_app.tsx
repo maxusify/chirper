759ffd17d0528f0e635df0b5c76adc8bb88fde52
maxusify <maxusify@gmail.com>
2023-03-23 11:29:30 +0100

    Cleanup after NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps,
-}) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
